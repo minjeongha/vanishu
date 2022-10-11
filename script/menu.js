988daabdfd18bd838043b9b8c61a90adaf3b2a44
minjeongha <113660654+minjeongha@users.noreply.github.com>
2022-10-11 17:13:54 +0900

    Delete script directory

--- script/menu.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.nav_inner> ul> li').mouseenter(function(){
-        $(this).children('.sub').stop().slideDown();
-    });
-    $('.nav_inner> ul> li').mouseleave(function(){
-        $(this).children('.sub').stop().slideUp();
-    });
-});
